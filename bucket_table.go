c49c1b340cdf29778deda951967e13c00c7c8c62
scalalang2 <cailhuiris@gmail.com>
2023-12-09 18:35:46 +0900

    refactored bucket table for ghost queue.

--- bucket_table.go
+++ bucket_table.go
@@ -1,31 +1,44 @@
 package fifo
 
+import "container/list"
+
 type bucketTable[K comparable] struct {
-	queue *ringBuf[K]
-	hash  map[K]bool
+	size  int
+	ll    *list.List
+	items map[K]*list.Element
 }
 
 func newBucketTable[K comparable](size int) *bucketTable[K] {
 	return &bucketTable[K]{
-		queue: newRingBuf[K](size),
-		hash:  make(map[K]bool),
+		size:  size,
+		ll:    list.New(),
+		items: make(map[K]*list.Element),
 	}
 }
 
 func (b *bucketTable[K]) add(key K) {
-	if b.queue.isFull() {
-		old := b.queue.pop()
-		delete(b.hash, old)
+	if _, ok := b.items[key]; ok {
+		return
 	}
 
-	b.queue.push(key)
-	b.hash[key] = true
+	for b.ll.Len() >= b.size {
+		e := b.ll.Back()
+		delete(b.items, e.Value.(K))
+		b.ll.Remove(e)
+	}
+
+	e := b.ll.PushFront(key)
+	b.items[key] = e
 }
 
 func (b *bucketTable[K]) remove(key K) {
-	delete(b.hash, key)
+	if e, ok := b.items[key]; ok {
+		b.ll.Remove(e)
+		delete(b.items, key)
+	}
 }
 
 func (b *bucketTable[K]) contains(key K) bool {
-	return b.hash[key]
+	_, ok := b.items[key]
+	return ok
 }
