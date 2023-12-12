12adbf9639c52b5d8f6cb6705c630a8aad39f88a
scalalang2 <cailhuiris@gmail.com>
2023-12-12 10:00:06 +0900

    Add unit test for SIEVE

---
+++ sieve_test.go
@@ -0,0 +1,45 @@
+package fifo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAndSetOnSieve(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	cache := NewSieve[int, int](10)
+
+	for _, v := range items {
+		cache.Set(v, v*10)
+	}
+
+	for _, v := range items {
+		val, ok := cache.Get(v)
+		require.True(t, ok)
+		require.Equal(t, v*10, val)
+	}
+}
+
+func TestContainsOnSieve(t *testing.T) {
+	cache := NewSieve[string, string](10)
+	require.False(t, cache.Contains("hello"))
+
+	cache.Set("hello", "world")
+	require.True(t, cache.Contains("hello"))
+}
+
+func TestLenOnSieve(t *testing.T) {
+	cache := NewSieve[int, int](10)
+	require.Equal(t, 0, cache.Len())
+
+	cache.Set(1, 1)
+	require.Equal(t, 1, cache.Len())
+
+	// duplicated keys only update the recent-ness of the key and value
+	cache.Set(1, 1)
+	require.Equal(t, 1, cache.Len())
+
+	cache.Set(2, 2)
+	require.Equal(t, 2, cache.Len())
+}
