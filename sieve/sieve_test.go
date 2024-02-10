628118229a8cd0e9c7469e5934d1b574be3c9f0f
scalalang2 <cailhuiris@gmail.com>
2024-02-10 17:11:22 +0900

    Add expirability for SIEVE

--- sieve/sieve_test.go
+++ sieve/sieve_test.go
@@ -2,13 +2,16 @@ package sieve
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetAndSetOnSieve(t *testing.T) {
+const noEvictionTTL = 0
+
+func TestGetAndSet(t *testing.T) {
 	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	cache := New[int, int](10)
+	cache := New[int, int](10, noEvictionTTL)
 
 	for _, v := range items {
 		cache.Set(v, v*10)
@@ -19,10 +22,12 @@ func TestGetAndSetOnSieve(t *testing.T) {
 		require.True(t, ok)
 		require.Equal(t, v*10, val)
 	}
+
+	cache.Close()
 }
 
-func TestRemoveOnSieve(t *testing.T) {
-	cache := New[int, int](10)
+func TestRemove(t *testing.T) {
+	cache := New[int, int](10, noEvictionTTL)
 	cache.Set(1, 10)
 
 	val, ok := cache.Get(1)
@@ -39,18 +44,55 @@ func TestRemoveOnSieve(t *testing.T) {
 	// This should not panic
 	removed = cache.Remove(-1)
 	require.False(t, removed)
+
+	cache.Close()
 }
 
-func TestContainsOnSieve(t *testing.T) {
-	cache := New[string, string](10)
+func TestSievePolicy(t *testing.T) {
+	cache := New[int, int](10, noEvictionTTL)
+	oneHitWonders := []int{1, 2, 3, 4, 5}
+	popularObjects := []int{6, 7, 8, 9, 10}
+
+	// add objects to the cache
+	for _, v := range oneHitWonders {
+		cache.Set(v, v)
+	}
+	for _, v := range popularObjects {
+		cache.Set(v, v)
+	}
+
+	// hit popular objects
+	for _, v := range popularObjects {
+		_, ok := cache.Get(v)
+		require.True(t, ok)
+	}
+
+	// add another objects to the cache
+	for _, v := range oneHitWonders {
+		cache.Set(v*10, v*10)
+	}
+
+	// check popular objects are not evicted
+	for _, v := range popularObjects {
+		_, ok := cache.Get(v)
+		require.True(t, ok)
+	}
+
+	cache.Close()
+}
+
+func TestContains(t *testing.T) {
+	cache := New[string, string](10, noEvictionTTL)
 	require.False(t, cache.Contains("hello"))
 
 	cache.Set("hello", "world")
 	require.True(t, cache.Contains("hello"))
+
+	cache.Close()
 }
 
-func TestLenOnSieve(t *testing.T) {
-	cache := New[int, int](10)
+func TestLen(t *testing.T) {
+	cache := New[int, int](10, noEvictionTTL)
 	require.Equal(t, 0, cache.Len())
 
 	cache.Set(1, 1)
@@ -62,4 +104,84 @@ func TestLenOnSieve(t *testing.T) {
 
 	cache.Set(2, 2)
 	require.Equal(t, 2, cache.Len())
+
+	cache.Close()
+}
+
+func TestPurge(t *testing.T) {
+	cache := New[int, int](10, noEvictionTTL)
+	cache.Set(1, 1)
+	cache.Set(2, 2)
+	require.Equal(t, 2, cache.Len())
+
+	cache.Purge()
+	require.Equal(t, 0, cache.Len())
+
+	cache.Close()
+}
+
+func TestTimeToLive(t *testing.T) {
+	ttl := time.Second
+	cache := New[int, int](10, ttl)
+	numberOfEntries := 10
+
+	for num := 1; num <= numberOfEntries; num++ {
+		cache.Set(num, num)
+		val, ok := cache.Get(num)
+		require.True(t, ok)
+		require.Equal(t, num, val)
+	}
+
+	time.Sleep(ttl * 2)
+
+	// check all entries are evicted
+	for num := 1; num <= numberOfEntries; num++ {
+		_, ok := cache.Get(num)
+		require.False(t, ok)
+	}
+}
+
+func TestEvictionCallback(t *testing.T) {
+	cache := New[int, int](10, noEvictionTTL)
+	evicted := make(map[int]int)
+
+	cache.SetOnEvict(func(key int, value int) {
+		evicted[key] = value
+	})
+
+	// add objects to the cache
+	for i := 1; i <= 10; i++ {
+		cache.Set(i, i)
+	}
+
+	// add another object to the cache
+	cache.Set(11, 11)
+
+	// check the first object is evicted
+	_, ok := cache.Get(1)
+	require.False(t, ok)
+	require.Equal(t, 1, evicted[1])
+
+	cache.Close()
+}
+
+func TestEvictionCallbackWithTTL(t *testing.T) {
+	cache := New[int, int](10, time.Second)
+	evicted := make(map[int]int)
+	cache.SetOnEvict(func(key int, value int) {
+		evicted[key] = value
+	})
+
+	// add objects to the cache
+	for i := 1; i <= 10; i++ {
+		cache.Set(i, i)
+	}
+
+	// wait for the objects to be evicted
+	time.Sleep(time.Second * 2)
+
+	// check the callback received all the evicted objects
+	for i := 1; i <= 10; i++ {
+		require.Equal(t, i, evicted[i])
+	}
 }
